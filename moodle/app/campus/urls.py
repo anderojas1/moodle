040c351dfd200174df9ea1db6bb3aaa24613f7d6
Anderson Enríquez <anderojas@anderojas-PC.home>
2015-04-23 00:26:15 -0500

    listar personas

--- moodle/app/campus/urls.py
+++ moodle/app/campus/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import include, url
 from .views import index, success
-from .views import RegistrarPersona
+from .views import RegistrarPersona, ListarPersonas
 
 urlpatterns = [
     url(r'^$', 'app.campus.views.index'),
     url(r'^success/$', 'app.campus.views.success', name='success'),
     url(r'^registrar/$', RegistrarPersona.as_view(), name='registrar_persona'),
+    url(r'^listados-personas/$', ListarPersonas.as_view(), name='listar_personas')
 ]
--- moodle/app/campus/views.py
+++ moodle/app/campus/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render_to_response
 from django.core.urlresolvers import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, ListView
 from .models import Persona
 
 def index(request):
@@ -15,3 +15,8 @@ class RegistrarPersona(CreateView):
 	model = Persona
 	fields = ['cedula', 'nombre', 'apellidos', 'fecha_nacimiento', 'sexo', 'email', 'celular', 'fijo']
 	success_url = reverse_lazy('success')
+
+class ListarPersonas(ListView):
+	template_name = 'campus/listados-personas.html'
+	model = Persona
+	context_object_name = 'personas'
