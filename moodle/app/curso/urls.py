2f272b862a687304621420fa20d2b3e76bd056de
Juan Sebastian <juan.sebastian.rivas@correounivalle.edu.co>
2015-05-04 21:38:51 -0500

    listado de cursos y cohortes

--- moodle/app/curso/urls.py
+++ moodle/app/curso/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import include, url
 from .views import success
-from .views import RegistrarCurso, RegistrarCohorte
+from .views import RegistrarCurso, RegistrarCohorte, ListarCursos, ListarCohortes
 
 urlpatterns = [
     url(r'^registrar-curso/$', RegistrarCurso.as_view(), name='registar_curso'),
     url(r'^registrar-cohorte/$', RegistrarCohorte.as_view(), name='registar_cohorte'),
+    url(r'^listar-curso/$', ListarCursos.as_view(), name='listar_cursos'),
+    url(r'^listar-cohorte/$', ListarCohortes.as_view(), name='listar_cohortes')
 ]
