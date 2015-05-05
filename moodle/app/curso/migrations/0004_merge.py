06dfe4f77ca43414cb357db29c984a6f8d04a56c
juliancv <julian.canacuan.valenzuela@correounivalle.edu.co>
2015-05-04 23:38:34 -0500

    Tabla bonita

---
+++ moodle/app/curso/migrations/0004_merge.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('curso', '0003_auto_20150504_2112'),
+        ('curso', '0003_auto_20150504_2056'),
+        ('curso', '0003_auto_20150504_2039'),
+    ]
+
+    operations = [
+    ]
